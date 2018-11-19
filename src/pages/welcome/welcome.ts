39283478fd3fa28f7e90b50cc2d2bd2a1b757fce
lviviana13 <lviviana13@gmail.com>
2018-11-19 12:38:33 -0500

    Agregar los métodos de registro e inicio de sesión

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { Nav, IonicPage, NavController, NavParams } from 'ionic-angular';
-
-
-
 import { RegistroPage } from '../registro/registro';
 import { HomePage } from '../home/home';
+import { MyservicesProvider } from '../../providers/myservices/myservices'; //Importar Servicio
+import { ToastController } from 'ionic-angular'; //Necesario para generar las notificaiones
 
 @IonicPage()
 @Component({
@@ -13,20 +12,49 @@ import { HomePage } from '../home/home';
 })
 export class WelcomePage {
 
+  //Objeto para almacenar los datos en login
+  autentificar = {
+  "nombreUsuario": "",
+  "clave": ""
+  }
+
+  //Información que obtendre del usuario al autentificar
+  usuario: any;
+
+
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public rest: MyservicesProvider,
+    public navParams: NavParams,
+    public toastCtrl: ToastController
   ) {
+    //this.username = autentificar.numIdentificacion;
+    //this.password = "";
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WelcomePage');
   }
 
-
-  iniciarSesion() {
-    console.log('ionViewDidLoad HomePage');
+  //Método para "autentificar" usuarios
+  inicioSesion() {
+    this.rest.getUsuario(this.autentificar.nombreUsuario, this.autentificar.clave).subscribe((respuesta) =>{
+      this.usuario = respuesta;
+    });
+    //Verficar que el usuario existe, comprobando que la respuesta es NO vacía
+    //Es necesario pasarle la condición de null porque sale error si no se agrega valores al usuario o clave
+    if (this.usuario == null || Object.keys(this.usuario).length === 0  ) {
+      console.log('No es posible obtener los datos del usuario');
+      //genera la notificación de registro NO exitoso
+      const toast = this.toastCtrl.create({
+      message: '¡Usuario o Contraseña Incorrectos! Verifique los datos ingresados',
+      duration: 4000
+      });
+      toast.present();
+    }else{
+    console.log('Usuario autentificado && ionViewDidLoad HomePage');
     this.navCtrl.setRoot(HomePage);
+    }
   }
 
   registro(){
