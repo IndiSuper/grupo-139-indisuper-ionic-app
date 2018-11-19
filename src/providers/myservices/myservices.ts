ef4f9752b4819b86f0a79fddff4d4510f78d3336
lviviana13 <lviviana13@gmail.com>
2018-11-19 12:42:17 -0500

    Definer los servicios de la API

--- src/providers/myservices/myservices.ts
+++ src/providers/myservices/myservices.ts
@@ -7,14 +7,33 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MyservicesProvider {
 
+  servidor: any;
+
   constructor(
     public http: HttpClient) {
     console.log('Hello MyservicesProvider Provider');
+    this.servidor="https://indisuper.glud.org/api/";
   }
+
+  //Método POST para registrar usuario
+  registrarUsuario(postData){
+    return this.http.post(this.servidor + 'Usuarios', postData);
+  }
+
+  //Método para autentificar usuario y obtener los datos del usuario
+  getUsuario(username, password){
+    let apiUser="https://indisuper.glud.org/api/Usuarios?filter={\"where\": {\"nickname\": \""+username+"\" , \"clave\": \""+password+"\"}}";
+    return this.http.get(encodeURI(apiUser));
+  }
+
+
+
+
+
 
   //Metodo para traer las id subcategotias relacionadas con EVENTOS
   getSubcategoriaEventos(){
-  return this.http.get('http://localhost:3000/api/CategoriasSubcategorias?filter=%7B%22where%22%3A%20%7B%22codCategoria%22%3A%201%7D%20%7D');
+  return this.http.get(this.servidor + 'CategoriasSubcategorias?filter=%7B%22where%22%3A%20%7B%22codCategoria%22%3A%201%7D%20%7D');
   }
 
   //Metodo para traer las id subcategotias relacionadas con CURSOS
@@ -25,20 +44,14 @@ export class MyservicesProvider {
 
   //ESTO SE QUEDA MIENTRAS LOGRAMOS HACER EL FILTRO
   getSubcategorias(){   //Metodo para traer las subcategorias, lo utilizamos en HomePage
-  return this.http.get('http://localhost:3000/api/Subcategorias');
+  return this.http.get(this.servidor + 'Subcategorias');
   }
 
   getEventos(){   //Metodo para traer las subcategorias, lo utilizamos en HomePage
-  return this.http.get('http://localhost:3000/api/Eventos');
+  return this.http.get('https://indisuper.glud.org/api/Eventos');
   }
 
-  getUsuario(){ //Obtener la información del Usuario
-  return this.http.get('http://localhost:3000/api/Usuarios/1');
-  }
 
-  //registrarUsuario(postData): {
-  //  return this.http.post('http://localhost:3000/api/Usuarios', postData);
-  //}
 
   //De aqui pa bajo son pruebas (también de arriba ja)... asi que esto no se tiene en cuenta haha
 /*
