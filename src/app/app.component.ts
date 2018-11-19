08bede6ea1f03f4edfc8f2384beea7bafc1b1f6b
lviviana13 <lviviana13@gmail.com>
2018-11-19 12:42:52 -0500

    establecer el boton para salir de la app, hay un bug con el menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,4 +66,10 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+
+  salir() {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(WelcomePage);
+  }
 }
